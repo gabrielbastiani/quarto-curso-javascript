36753231d639879ed7ec1c81fa9d61cc6a84ca80
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2021-11-01 20:43:06 -0300

    strings

---
+++ LOGICA-PROGRAMACAO/tipos-primitivos.js
@@ -0,0 +1,15 @@
+// strings, number (inteiros, floats), boolean
+
+let minhaVar = "minha string";
+let minhavar2 = 'minha "string" com aspas simples';
+console.log(minhavar2)
+
+var minhavar3 = `minha template literal`;
+console.log(minhavar3);
+
+let idade = 40;
+/* let msg = "eu possuo " + idade + " anos"; */
+let msg = `eu possuo ${idade} anos`;
+
+console.log(msg);
+console.log("Hello " + " world");
--- LOGICA-PROGRAMACAO/variaveis.js
+++ LOGICA-PROGRAMACAO/variaveis.js
@@ -1,3 +1,10 @@
 // var -> ES5 - Para funcionar em todos os browser
 
 // let e const -> ES2015
+
+let teste = "minha string";
+teste = 101;
+console.log(teste)
+
+const teste2 = 10;
+console.log(teste2);
