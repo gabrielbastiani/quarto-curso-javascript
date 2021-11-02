ee7d957dc96d3668fb8d4b80f37581f89bd8f70a
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2021-11-02 11:35:09 -0300

    atualizacao

--- LOGICA-PROGRAMACAO/operadores.js
+++ LOGICA-PROGRAMACAO/operadores.js
@@ -1,9 +1,38 @@
 // + - * / % **
 let n1 = 10
 let n2 = 5
+
+//OPERADORES ARITIMETICOS
 console.log(n1 + n2)
 console.log(n1 - n2)
 console.log(n1 * n2)
 console.log(n1 / n2)
 console.log(n1 % n2)
 console.log(2 ** 3)
+
+// OPERADORES DE ATRIBUIÇÃO
+let n3 = 20
+// n3 = n3 + 15;
+n3 -= 15;
+console.log(n3)
+
+// INCREMENTO E DECREMENTO
+let i = 0;
+console.log(i++);
+console.log(i);
+
+// COMPARAÇÃO
+/*
+igualdade de valor ==
+igualdade de valor e tipo ===
+<, >, <=, >=
+!= valores diferentes
+!== valores e tipos diferentes
+*/
+
+console.log(n1, n2, n3);
+console.log(n1 < 10);
+console.log(n1 >= 10);
+
+console.log(n1 != 10);
+console.log(n1 !== "10");
