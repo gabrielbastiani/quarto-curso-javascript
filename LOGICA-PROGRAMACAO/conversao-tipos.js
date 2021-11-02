0d0723112fbfa7986e3cb349f0e0cc3f730d8b5c
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2021-11-01 21:37:34 -0300

    numbers

---
+++ LOGICA-PROGRAMACAO/conversao-tipos.js
@@ -0,0 +1,14 @@
+/* let n1 = 10;
+let n2 = "2"; */
+
+/* let n1 = "10.4353453";
+n1 = parseFloat(n1); */
+
+let n1 = "14";
+n1 = Number(n1);
+
+let n2 = 2;
+
+console.log(n1 * n2, typeof n1, typeof n2, n1);
+
+// parseInt, ParseFloat, Number()
--- LOGICA-PROGRAMACAO/tipos-primitivos.js
+++ LOGICA-PROGRAMACAO/tipos-primitivos.js
@@ -1,4 +1,5 @@
 // strings, number (inteiros, floats), boolean
+// undefined, null, symbol (ES2015)
 
 let minhaVar = "minha string";
 let minhavar2 = 'minha "string" com aspas simples';
@@ -13,3 +14,18 @@ let msg = `eu possuo ${idade} anos`;
 
 console.log(msg);
 console.log("Hello " + " world");
+console.log(typeof msg, typeof idade, typeof minhaVar);
+
+const n1 = 10
+const n2 = 1.1
+console.log(`o tipo de n1 é ${typeof n1} e seu valor é ${n1}`);
+console.log(`o tipo de n1 é ${typeof n2} e seu valor é ${n2}`);
+
+const isValid = true;
+console.log(`isValid: ${isValid}`);
+
+let varTeste
+console.log(varTeste);
+console.log(typeof varTeste);
+varTeste = 10;
+console.log(typeof varTeste, varTeste);
