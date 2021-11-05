923ea0ed7aebc28b9cb72618351dabc7c6567665
Gabriel Campos de Bastiani <gabriel.bastiani@sumig.local>
2021-11-05 16:59:51 -0300

    novas aulas

--- LOGICA-PROGRAMACAO/sum.js
+++ LOGICA-PROGRAMACAO/sum.js
@@ -1,3 +1,18 @@
 function sum(n1, n2){
+    if(typeof n1 !== "number" || typeof n2 !== "number"){
+        throw Error("sum aceita apenas numeros")
+    }
     return n1 + n2
 }
+
+let soma = ""
+try {
+    soma = sum(3, "a")
+} catch (e) {
+    console.log("Ocorreu um erro")
+    console.log(e.message)
+} finally {
+    console.log("bloco finaly")
+}
+
+console.log(soma)
---
+++ introducao/introducao-pratica/primeiro-exemplo.js
@@ -0,0 +1,20 @@
+console.log("Javascript funcionando 2222");
+
+
+/*
+let nome = "Gabriel"
+console.log(10 + 20)
+*/
+
+console.log("qualquer coisas")
+
+// uma unica linha
+
+let nome = "Gabriel"; alert(nome)
+
+const hasAttrubite = ""
+
+
+
+
+
